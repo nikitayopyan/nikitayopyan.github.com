bfd856b6af541196e87d506a70f70f2030e63a80
Oleg <jopa@pizda.com>
2020-07-22 18:07:57 +0300

    uploading

---
+++ pulse/js/script.js
@@ -0,0 +1,115 @@
+const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    controls: false,
+    nav: false
+  });
+
+
+    document.querySelector('.prev').addEventListener('click', function () {
+    slider.goTo('prev');
+});
+document.querySelector('.next').addEventListener('click', function () {
+    slider.goTo('next');
+});
+
+$(function() {
+    
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+    $(this)
+        .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+        .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+    
+});
+
+function toggleSlide(item){
+    $(item).each(function(i){
+        $(this).on('click', function(e){
+            e.preventDefault();
+            $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active')
+            $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active')
+        })
+    })
+}
+toggleSlide('.catalog-item__link')
+toggleSlide('.catalog-item__back')
+
+$('[data-modal=consultation]').on('click', function(){
+    $('.overlay, #consultation').fadeIn();
+});
+$('.modal__close').on('click', function(){
+    $('.overlay, #consultation, #order, #thanks').fadeOut();
+});
+$('.button_mini').each(function(i){
+    $(this).on('click', function(){
+        $('#order .modal__descr').text($('.catalog-item__subheader').eq(i).text());
+        $('.overlay, #order').fadeIn();
+    });
+});
+
+function validateForms(form){
+    $(form).validate({
+        rules: {
+            name: {
+                required: true,
+                minlength: 2
+            },
+            phone: "required",
+            email: {
+                required: true,
+                email: "true"
+            }
+        },
+        messages: {
+            name: {
+                required: "Enter your name",
+                minlength: jQuery.validator.format("Please enter {0} characters!")
+            },
+            phone: "Please enter your phone",
+            email: {
+                required: "Enter your email",
+                email: "Enter a valid email"
+            }
+        }
+    });
+};
+validateForms("#consultation-form");
+validateForms("#consultation form");
+validateForms("#order form");
+
+$('input[name=phone]').mask("+ 380(99) 999-9999");
+
+$('form').submit(function(e) {
+    e.preventDefault();
+    $.ajax({
+        type: "POST",
+        url: "mailer/smart.php",
+        data: $(this).serialize()
+    }).done(function(){
+        $(this).find("input").val("");
+        $('#consultation, #order').fadeOut();
+        $('.overlay, #thanks').fadeIn('slow');
+        $('form').trigger('reset');
+    });
+    return false;
+});
+
+$(window).scroll(function(){
+    if($(this).scrollTop() > 1600) {
+        $('.pageup').fadeIn();
+    }
+    else {
+        $('.pageup').fadeOut();
+    }
+});
+
+new WOW().init();
+
+$("a[href^='#']").click(function(){
+    var _href = $(this).attr("href");
+    $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+    return false; 
+});
