1ffc138b5511689caac1f02e990b05ab1bd95279
Oleg <jopa@pizda.com>
2020-07-27 15:07:59 +0300

    new proj

---
+++ consoleapp/js/script.js
@@ -0,0 +1,58 @@
+// "use strict";
+
+// let string = 'holy ';
+// let shit = 'shit';
+
+// console.log(string + shit);
+
+// let bool = true; 
+
+
+// const human = {
+//     name: 'Grisha',
+//     age: 40,
+//     isMarried: false
+// };
+
+// console.log(human.isMarried);
+
+// let arr = ['jopa.jpeg', 2, 'pizda.png'];
+
+// console.log(arr[0]);
+
+// const result = confirm("are you here?");
+// console.log(typeof(result));
+
+// const answer = +prompt("Tebe est 18?", "Pizda");
+// console.log(typeof(answer));
+
+// const answers = [];
+
+// answers[0] = prompt('как вас зовут?', "daun");
+// answers[1] = prompt('какая у вас фамилия?', "daun");
+// answers[2] = prompt('сколько лет', "18");
+
+// document.write(answers);
+
+// let incr = 10,
+//     decr = 10;
+
+// ++incr;
+// --decr;
+// console.log(incr);
+// console.log(decr);
+
+// const isChecked = true,
+//       isClosed = false;
+
+// console.log(isChecked || isClosed);
+
+let numberOfFilms = prompt("Сколько фильмов Вы уже посморели?", "");
+
+let personalMovieDB = ['count', 'movies', 'actors', 'genres', 'privat'];
+
+personalMovieDB[0].log(numberOfFilms);
+
+
+
+
